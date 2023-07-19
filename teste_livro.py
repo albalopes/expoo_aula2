ba87492e34496543a6a3f28d47aee51a6e0e1e54
albalopes <alba.lopes@ifrn.edu.br>
2023-07-19 20:02:07 +0000

    Modificando o arquivo teste_livro.py

--- teste_livro.py
+++ teste_livro.py
@@ -2,3 +2,6 @@ from livro import Livro
 
 novolivro = Livro('Orgulho e Preconceito', 'Jane Austen', '1890')
 novolivro.info()
+
+outrolivro = Livro('1984', 'George Orwell', '1950')
+outrolivro.info()
